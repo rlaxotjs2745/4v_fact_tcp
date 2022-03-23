da450db44b9ecb9d915d141135f6b7773e83b53c
jungbin-lee <hihihi486@naver.com>
2022-03-23 14:09:50 +0900

    e

---
+++ src/main/java/com/example/shinhandata/ShinhanDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinhandata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinhanDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shinhandata/ShinhanDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinhandata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
