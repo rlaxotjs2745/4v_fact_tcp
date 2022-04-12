788a059da376aa2ca3daa58ab89fbcacc6c40323
teaseon <taeseon@4thevision.com>
2022-04-12 14:06:08 +0900

    init env data tcp

--- src/main/java/com/example/shinhandata/DbConnection.java
+++ src/main/java/com/example/shinhandata/DbConnection.java
@@ -10,15 +10,25 @@ public class DbConnection {
     public static Connection getConnection() {
         Connection connect = null;
         try{
-            String user = "c##fact_usr";
-            String pw = "pass";
-            String url = "jdbc:oracle:thin:@192.168.0.97:1521:orcl";
-//
+////            dev
+//            String user = "c##fact_usr";
+//            String pw = "pass";
+//            String url = "jdbc:oracle:thin:@192.168.0.97:1521:orcl";
+
+//          상주
+            String user = "fact_user";
+            String pw = "fact1230";
+            String url = "jdbc:tibero:thin:@192.168.50.13:8629:SFINNOV";
+
+////            김제
 //            String user = "fact_user";
 //            String pw = "fact1230";
-//            String url = "jdbc:tibero:thin:@192.168.50.13:8629:SFINNOV";
+//            String url = "jdbc:tibero:thin:@192.168.51.13:8629:SFINNOV";
 
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("com.tmax.tibero.jdbc.TbDriver");
+
+            System.out.println("gdgd");
             connect = DriverManager.getConnection(url, user, pw);
         } catch (ClassNotFoundException e){
             System.out.println("드라이버 실패 : " + e.toString());
